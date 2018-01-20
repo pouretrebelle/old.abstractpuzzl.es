1eff8c432fab4a84588af3cb9f0a9c47dd0c4ae6
Charlotte Dann <ask@pouretrebelle.com>
2018-01-20 17:33:25 +0000

    :recycle: Refactor background js (#2)
    * Use some ES6
    
    * Make Dot a class
    
    * Rename some variables

--- src/background.js
+++ src/background.js
@@ -1,133 +1,128 @@
 import Vector2 from './utils/Vector2';
 import { random, randomInteger, clamp } from './utils/numberUtils';
 
-function hsl(h, s, l) { return 'hsl('+h+', '+clamp(s,0,100)+'%, '+clamp(l,0,100)+'%)';};
-
+function hsl(h, s, l) { return `hsl(${h}, ${clamp(s,0,100)}%, ${clamp(l,0,100)}%)`;};
 
 let	screenWidth = window.innerWidth;
 let screenHeight = window.innerHeight;
 let screenMin = (screenWidth < screenHeight) ? screenWidth : screenHeight;
 
-const options = {
-	background: '#fff',
-  predraw: 200,
-  color: '#000',
-  count: screenWidth * screenHeight * 0.00005,
-};
 const prettyHues = [2, 10, 17, 37, 40, 63, 67, 72, 74, 148, 152, 156, 160, 170, 175, 189, 194, 260, 270, 280, 288, 302, 320, 330, 340, 350];
 
-var canvas,
-		c;
-var dots = [];
-
-function setup() {
-  window.frameRate = 30;
-  setupCanvas();
-	c.fillStyle = options.background;
-	c.strokeStyle = options.color;
-  c.fillRect(-screenWidth/2, -screenHeight/2, screenWidth, screenHeight);
-  makeDots(options.count);
+const background = {
+  c: undefined,
+  dots: [],
+  backgroundColor: '#fff',
+  dotCount: screenWidth * screenHeight * 0.00005,
+  dotPredraws: 200,
 }
 
-function makeDots(count) {
+const setup = () => {
+  setupCanvas();
+  makeDots(background.dotCount);
+}
+
+const makeDots = (count) => {
   for (var i = 0; i < count; i++) {
     var x = randomInteger(-screenWidth*0.5,screenWidth*0.5);
     var y = randomInteger(-screenHeight*0.5,screenHeight*0.5);
 
     const dot = new Dot(x, y, i);
 
-    let tempPredraw = options.predraw;
+    let tempPredraw = background.dotPredraws;
     while (tempPredraw > 0) {
-      dot.update(canvas);
-      dot.draw(c);
+      dot.update();
+      dot.draw();
       tempPredraw--;
     }
 
-    dots.push(dot);
+    background.dots.push(dot);
   }
 }
 
-function draw() {
-  for (var i = 0; i < dots.length; i++) {
-    var dot = dots[i];
-    dot.update(canvas);
-    dot.draw(c);
-  }
+const draw = () => {
+  background.dots.forEach((dot) => {
+    dot.update();
+    dot.draw();
+  });
 }
 
-function setupCanvas() {
-  canvas = document.createElement('canvas');
-  c = canvas.getContext('2d');
+const setupCanvas = () => {
+  const canvas = document.createElement('canvas');
   canvas.width = screenWidth;
   canvas.height = screenHeight;
   document.body.appendChild(canvas);
-  c.translate(screenWidth/2, screenHeight/2);
+
+  background.c = canvas.getContext('2d');
+	background.c.fillStyle = background.backgroundColor;
+  background.c.translate(screenWidth/2, screenHeight/2);
+  background.c.fillRect(-screenWidth/2, -screenHeight/2, screenWidth, screenHeight);
 }
 
+class Dot {
 
-const Dot = function(x, y, index) {
+  constructor(x, y, index) {
+    const hue = (prettyHues[randomInteger(0, prettyHues.length-1)]-20)%360;
+    const sat = randomInteger(80, 100);
+    const bri = randomInteger(65, 75);
+    this.color = hsl(hue, sat, bri);
+    this.shadow = hsl((hue+40)%360, sat, bri);
 
-  var hue = (prettyHues[randomInteger(0, prettyHues.length-1)]-20)%360;
-  var sat = randomInteger(80, 100);
-  var bri = randomInteger(65, 75);
-  var speed = random(1, 3);
+    this.size = randomInteger(25, 50);
+    this.pos = new Vector2(x,y);
+    this.vel = new Vector2(random(1, 3),0);
+    this.ang = 180-this.pos.angle();
+    this.rot = random(0.01,0.08);
+    this.dir = randomInteger(0,1);
 
-  var size = this.size = randomInteger(25, 50);
-	var pos = this.pos = new Vector2(x,y);
-	var vel = this.vel = new Vector2(speed,0);
-	var ang = this.ang = 180-pos.angle();
-  var rot = this.rot = random(0.01,0.08);
-	var dir = this.dir = randomInteger(0,1);
+    this.vel.rotate(this.ang);
+  }
 
-	vel.rotate(ang);
-
-  this.color = hsl(hue, sat, bri);
-  this.shadow = hsl((hue+40)%360, sat, bri);
-
-  this.update = function(canvas) {
-
+  update() {
 		// randomly change clockwiseness
 		if (Math.random() < 0.01) {
-			dir = (dir == 1) ? 0 : 1;
+			this.dir = (this.dir == 1) ? 0 : 1;
 		}
 
 		// rotate
-		if (dir) {
-			vel.rotate(this.rot);
+		if (this.dir) {
+			this.vel.rotate(this.rot);
 		} else {
-			vel.rotate(-this.rot);
-		}
+			this.vel.rotate(-this.rot);
+    }
+
 		// add velocity to position
-		pos.plusEq(vel);
+		this.pos.plusEq(this.vel);
   };
 
-  this.draw = function(c) {
-    c.save();
+  draw(c) {
+    background.c.save();
 
-    c.fillStyle = this.shadow;
-		c.beginPath();
-      c.arc(pos.x,pos.y+5,this.size,0,Math.PI*2,true);
-		c.fill();
+    // draw shadow dot
+    background.c.fillStyle = this.shadow;
+		background.c.beginPath();
+      background.c.arc(this.pos.x,this.pos.y+5,this.size,0,Math.PI*2,true);
+		background.c.fill();
 
-    c.fillStyle = this.color;
-		c.beginPath();
-      c.arc(pos.x,pos.y,this.size,0,Math.PI*2,true);
-		c.fill();
+    // draw upper dot
+    background.c.fillStyle = this.color;
+		background.c.beginPath();
+      background.c.arc(this.pos.x,this.pos.y,this.size,0,Math.PI*2,true);
+		background.c.fill();
 
-    c.restore();
+    background.c.restore();
   };
 
 };
 
-
-function loop() {
+const loop = () => {
   draw();
   window.requestAnimationFrame(loop);
 }
 
-window.addEventListener('load', init);
-
-function init() {
+const init = () => {
   setup();
   window.requestAnimationFrame(loop);
 }
+
+window.addEventListener('load', init);
