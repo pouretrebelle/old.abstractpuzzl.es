614a06c83e86b3823afe0fdbfdbd5bf114934cf2
Charlotte Dann <ask@pouretrebelle.com>
2018-10-29 15:19:24 +0000

    :rocket:

--- docs/main.js
+++ docs/main.js
@@ -61,7 +61,7 @@
 /******/ 	
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "8f7b395e593e0a91ec06"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "f1891b8fc4db7d5cdb85"; // eslint-disable-line no-unused-vars
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule; // eslint-disable-line no-unused-vars
